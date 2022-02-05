3d26757fa56100232e51ac540347d5f4978ea60f
Adedayo13 <99080001+Adedayo13@users.noreply.github.com>
2022-02-05 13:30:07 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var first_name = "Ranbir ";
+var last_name = "Kapoor";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var first_number = "10";
+var second_number = "5";
+var both_numbers = first_number.concat(second_number);
+console.log(both_numbers);
